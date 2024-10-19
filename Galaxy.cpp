c035ceb4ba216011fc477688fd03fd625216ddc9
leolin2810 <91626808+leolin2810@users.noreply.github.com>
2024-10-19 16:19:48 -0400

    Merged all changes
    Qiang's code for probe incorporated into main
    Fixed area value of default constructor to -1
    Created new constructor to take in variables
    Merged Probe1.cpp and Probe.cpp
    Merged Probe.hpp and Probe.h
    Created header file for Galaxy class

---
+++ Galaxy.cpp
@@ -0,0 +1,23 @@
+#include "Galaxy.h"
+#include <iostream>
+void Galaxy::addProbe()
+{
+    // gala[numProbes] = Probe 
+}
+
+Probe Galaxy::returnProbe(int index)
+{
+    return Probe();
+}
+
+void Galaxy::sortByName()
+{
+}
+
+void Galaxy::sortByID()
+{
+}
+
+void Galaxy::sortByArea()
+{
+}
--- Galaxy.h
+++ Galaxy.h
@@ -1,7 +1,22 @@
 #ifndef GALAXY_H
 #define GALAXY_H
+#include "Probe.h"
+#include <vector>
+
+using namespace std;
+
 
 class Galaxy {
+private:
+    vector<Probe> gala;
+    int numProbes;
+public:
+    void addProbe();
+    Probe returnProbe(int index);
+    void sortByName();
+    void sortByID();
+    void sortByArea();
+
 
 };
 
