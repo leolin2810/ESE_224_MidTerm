6f81bd809ed0b8c2a9da30dd3f1784bce78bf378
leolin2810 <91626808+leolin2810@users.noreply.github.com>
2024-10-19 16:23:13 -0400

    Merge (Cont)

--- Probe.cpp
+++ Probe.cpp
@@ -5,6 +5,12 @@ Probe::Probe() : name(""), ID(-1), dimension{-1, -1}, area(-1), position{-1, -1}
 {
 }
 
+Probe::Probe(std::string n, int id, int length, int width, int x, int y)
+    : name(n), ID(id), dimension{length, width}, position{x, y}
+{
+    calculateArea();
+}
+
 void Probe::testPrint()
 {
     std::cout << name << std::endl
@@ -13,4 +19,88 @@ void Probe::testPrint()
     //   << dimension << std::endl
     std::cout << "(" << position[0] << ", " << position[1] << ")" << std::endl;
     //   << position << std::endl;
+}
+
+// Accessors
+std::string Probe::getName() const
+{
+    return name;
+}
+
+int Probe::getID() const
+{
+    return ID;
+}
+
+int Probe::getDimension(int index) const
+{
+    return dimension[index];
+}
+
+int Probe::getPosition(int index) const
+{
+    return position[index];
+}
+
+double Probe::getArea() const
+{
+    return area;
+}
+
+// Mutators
+void Probe::setName(std::string n)
+{
+    name = n;
+}
+
+void Probe::setID(int id)
+{
+    ID = id;
+}
+
+void Probe::setDimension(int index, int value)
+{
+    dimension[index] = value;
+    calculateArea(); // Automatically recalculate area when dimensions are changed
+}
+
+void Probe::setPosition(int index, int value)
+{
+    position[index] = value;
+}
+
+// Method to calculate the area
+void Probe::calculateArea()
+{
+    area = 2 * (dimension[0] + dimension[1]);
+}
+
+// Display probe information
+void Probe::displayProbe() const
+{
+    std::cout << "Name: " << name << "\nID: " << ID << "\nDimensions: "
+              << dimension[0] << "x" << dimension[1] << "\nArea: " << area
+              << "\nPosition: (" << position[0] << ", " << position[1] << ")\n";
+}
+
+// Overloaded operators
+void Probe::operator<<(const Probe &p2)
+{
+    name = p2.name;
+    ID = p2.ID;
+    dimension[0] = p2.dimension[0];
+    dimension[1] = p2.dimension[1];
+    position[0] = p2.position[0];
+    position[1] = p2.position[1];
+    area = p2.area;
+}
+
+void Probe::operator-(Probe &p2)
+{
+    std::swap(dimension[0], p2.dimension[0]);
+    std::swap(dimension[1], p2.dimension[1]);
+    std::swap(position[0], p2.position[0]);
+    std::swap(position[1], p2.position[1]);
+    calculateArea();
+    p2.calculateArea();
 }
--- Probe.h
+++ Probe.h
@@ -10,7 +10,29 @@ private:
 
 public:
     Probe();
+    Probe(std::string n, int id, int length, int width, int x, int y);
     void testPrint();
+    
+    // Accessors
+    std::string getName() const;
+    int getID() const;
+    int getDimension(int index) const;
+    int getPosition(int index) const;
+    double getArea() const;
+
+    // Mutators
+    void setName(std::string n);
+    void setID(int id);
+    void setDimension(int index, int value);
+    void setPosition(int index, int value);
+
+    // Methods
+    void calculateArea();
+    void displayProbe() const;
+
+    // Overloaded operators
+    void operator<<(const Probe& p2);
+    void operator-(Probe& p2);
     
 };
 
